d3c7c416b06657862ab0baa36a47e87798ca2682
Giorgos Ampavis <giorgos.ampavis>
2021-03-25 18:50:58 +0000

    new: Added solution to Print all paths in a binary tree

---
+++ Swift-Algorithms.playground/Pages/Print all paths in binary tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,57 @@
+//: [Previous](@previous)
+
+/*
+ You have a binary tree:
+     A
+    B C
+   D E F
+ 
+ Print all paths of the tree from root to leaf
+
+ Output:
+ ABD
+ ACE
+ ACF
+*/
+
+import Foundation
+
+class Node {
+    init(value: String){
+        self.value = value
+    }
+ 
+    var value: String
+    var childNodes  = [Node]()
+    
+    var isLeafNode: Bool {
+        return childNodes.count > 0 ? false : true
+    }
+}
+
+var root = Node(value: "A")
+var bNode = Node(value: "B")
+var cNode = Node(value: "C")
+var dNode = Node(value: "D")
+var eNode = Node(value: "E")
+var fNode = Node(value: "F")
+bNode.childNodes = [dNode]
+cNode.childNodes = [eNode, fNode]
+root.childNodes = [bNode, cNode]
+
+var path = [String]()
+printPaths(node: root, path: &path)
+
+func printPaths(node: Node?, path: inout [String]) {
+    guard let node = node else{
+        return
+    }
+    path.append(node.value)
+    if node.isLeafNode {
+        print(path)
+    }
+    for n in node.childNodes {
+        printPaths(node: n, path: &path)
+    }
+    path.removeLast()
+}
